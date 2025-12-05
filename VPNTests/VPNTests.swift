1fd695a8a7a3c4ae7a4d696437365abc0b3162b8
Jackie <j2ckthebest@gmail.com>
2025-12-05 11:08:43 +0100

    Initial Commit

---
+++ VPNTests/VPNTests.swift
@@ -0,0 +1,17 @@
+//
+//  VPNTests.swift
+//  VPNTests
+//
+//  Created by DhahabwhBb Jshehaha on 12/4/25.
+//
+
+import Testing
+@testable import VPN
+
+struct VPNTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
